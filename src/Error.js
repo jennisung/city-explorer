8d2b70d6e21abf795761c5dbf3b817c0c548db53
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-12 22:13:53 -0400

    adding alert

--- src/Error.js
+++ src/Error.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
 
 class Error extends React.Component {
-  render(){
-    return <h1>Error!</h1>
+  render() {
+    return (
+      <>
+        <Alert variant="warning">
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>{this.props.errorMessage}</p>
+        </Alert>
+      </>
+    );
   }
 }
 
 export default Error;
+
--- src/Location.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Location extends React.Component {
-  render(){
-    return <h1>Location!</h1>
-  }
-}
-
-export default Location;
