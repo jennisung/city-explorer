39cffce5b08a1e4af8ca04a60347922936d6f092
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-14 19:02:48 -0400

    add bootstrap to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class Weather extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>City Weather</h2>
-        {this.props.forecastData.map((day, index) => (
-          <div key={index} className="weather-div">
-            <div className="weather-card">
-              <p>Date: {day.date}</p>
-              <p>Description: {day.description}</p>
+        <div className="row">
+          {this.props.forecastData.map((day, index) => (
+            <div key={index} className="">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Date: {day.date}</Card.Title>
+                  <Card.Text>Description: {day.description}</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     );
   }
