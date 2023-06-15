05772cda72f9360a9f8ca5fe7816bc6cafca1084
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-14 23:49:06 -0400

    adding movies handler

---
+++ src/Movies.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Movies extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Movies</h2>
+        {this.props.moviesData.map((movie, index) => (
+          <div key={index}>
+            <h3>{movie.title}</h3>
+            <p>{movie.overview}</p>
+            <p>Average Votes: {movie.average_votes}</p>
+            <p>Total Votes: {movie.total_votes}</p>
+            <img src={movie.imageUrl} alt={movie.title} />
+            <p>Popularity: {movie.popularity}</p>
+            <p>Released: {movie.released}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Movies;
