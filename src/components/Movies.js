fd6c491b24c91842a009d7b3b4b4335b5d45a2e3
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-15 22:42:39 -0400

    fixing styling

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Movies extends React.Component {
   render() {
-    let moviesData = this.props.moviesData.slice(0, 3); 
+    let moviesData = this.props.moviesData.slice(0, 20);
 
     return (
-      <div>
-        <h2>Movies</h2>
-        {moviesData.map((movie, index) => (
-          <div key={index}>
-            <h3>{movie.title}</h3>
-            <p>{movie.overview}</p>
-            <p>Average Votes: {movie.average_votes}</p>
-            <p>Total Votes: {movie.total_votes}</p>
-            <img src={movie.imageUrl} alt={movie.title} />
-            <p>Popularity: {movie.popularity}</p>
-            <p>Released: {movie.released}</p>
-          </div>
+    <div>
+      <h2>List of Movies</h2>
+    <Carousel>
+      {moviesData.map((movie, index) => (
+      <Carousel.Item key={index}>
+      <img src={movie.imageUrl} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>{movie.overview}</p>
+      <p>Average Votes: {movie.average_votes}</p>
+      <p>Total Votes: {movie.total_votes}</p>
+      <p>Popularity: {movie.popularity}</p>
+      <p>Released: {movie.released}</p>
+        </Carousel.Item>
         ))}
-      </div>
+      </Carousel>
+    </div>
     );
   }
 }
 
 export default Movies;
+
 
 // import React from 'react';
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,29 +3,39 @@ import Card from 'react-bootstrap/Card';
 
 class Weather extends React.Component {
   render() {
-    let forecastData = this.props.forecastData.slice(0, 1); 
+    let forecastData = this.props.forecastData.slice(0, 16);
+    let randomIndex = Math.floor(Math.random() * forecastData.length);
+    let randomDay = forecastData[randomIndex];
+
+    let WeatherDay = ({ date, description }) => (
+    <div>
+      <Card>
+        <Card.Body>
+          {<h2>City Weather</h2>}
+          <Card.Title>Date: {date}</Card.Title>
+          <Card.Text>Description: {description}</Card.Text>
+        </Card.Body>
+      </Card>
+     </div>
+    );
 
     return (
-      <div className="container">
-        <h2>City Weather</h2>
-        <div className="row">
-          {forecastData.map((day, index) => (
-            <div key={index} className="">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Date: {day.date}</Card.Title>
-                  <Card.Text>Description: {day.description}</Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          ))}
-        </div>
+      <div>
+          <WeatherDay
+            heading={<h2>City Weather</h2>}
+            key={randomIndex}
+            date={randomDay.date}
+            description={randomDay.description}
+          />
       </div>
     );
   }
 }
 
 export default Weather;
+
+
+
 
 // import React from 'react';
 // import Card from 'react-bootstrap/Card';
