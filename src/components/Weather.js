734d8d69ce0f8c7cb4f3bad1814f4e27b3681fc7
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-18 16:06:33 -0400

    refactor

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,80 +1,32 @@
 import Table from 'react-bootstrap/Table';
 import React from 'react';
 import '../styles/Weather.css';
-
+import WeatherDay from './Weatherday';
 
 class Weather extends React.Component {
   render() {
-  let sevenDayForecast = this.props.forecastData.slice(0, 7);
     return (
       <div className="div-weather">
-        {/* <h2 >City Weather</h2> */}
         <Table className='weather-table' striped bordered hover>
           <thead>
-           <tr>
-           <td colSpan={2}> <h2 className='city-weather-heading'>7 day forecast</h2> </td>     
-           </tr>
-
+          <tr>
+              <td colSpan={2}>
+                <h2 className='city-weather-heading'>7-day forecast</h2>
+              </td>
+            </tr>
             <tr>
-               <th>Date</th>
-               <th>Description</th>
+              <th>Date</th>
+              <th>Description</th>
             </tr>
           </thead>
           <tbody>
-
-            {sevenDayForecast.map((day, index) => (
-              <tr key={index}>
-              <td>{day.date}</td>
-              <td>{day.description}</td>
-              </tr>
-            ))
-            }
-
+           
+            <WeatherDay forecastData={this.props.forecastData} />
           </tbody>
         </Table>
       </div>
     );
   }
-};
-
+}
 
 export default Weather;
-
-// import React from 'react';
-// import Card from 'react-bootstrap/Card';
-// import Table from 'react-bootstrap/Table';
-
-
-// class Weather extends React.Component {
-//   render() {
-//     let forecastData = this.props.forecastData.slice(0, 5);
-//     let randomIndex = Math.floor(Math.random() * forecastData.length);
-//     let randomDay = forecastData[randomIndex];
-
-//     let WeatherDay = ({ date, description }) => (
-//     <div>
-//       <Card>
-//         <Card.Body>
-//           {<h2>City Weather</h2>}
-//           <Card.Title>Date: {date}</Card.Title>
-//           <Card.Text>Description: {description}</Card.Text>
-//         </Card.Body>
-//       </Card>
-//      </div>
-//     );
-
-//     return (
-//       <div>
-//           <WeatherDay
-//             heading={<h2>City Weather</h2>}
-//             key={randomIndex}
-//             date={randomDay.date}
-//             description={randomDay.description}
-//           />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Weather;
-
